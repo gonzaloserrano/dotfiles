6c5f269da2ec3c420a257f8de2e3fbe4c37323b1
gonzaloserrano <boikot@gmail.com>
2024-11-11 21:29:54 +0100

    Try buf's buf_ls langserver for protobuf

--- nvim/ftplugin/proto.lua
+++ nvim/ftplugin/proto.lua
@@ -3,3 +3,18 @@ set.expandtab = true
 set.shiftwidth = 2
 set.tabstop = 2
 set.softtabstop = 2
+
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+end
+
+-- Set up completion
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+require('lspconfig')['buf_ls'].setup{
+  on_attach = on_attach,
+  flags = lsp_flags,
+  capabilities = capabilities,
+  filetypes = {"proto"},
+}
